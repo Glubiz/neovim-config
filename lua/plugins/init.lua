89aba28cb2920c1b6eca876170dc8ef6ea4424db
Jonathan Solskov Jensen <josj@cego.dk>
2024-05-24 11:39:42 +0200

    Adding harpoon

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,6 @@
 return {
   { import = "plugins.specs.crates" },
+  { import = "plugins.specs.harpoon" },
   { import = "plugins.specs.indent-blankline" },
   { import = "plugins.specs.lspsaga" },
   { import = "plugins.specs.rustaceanvim" },
