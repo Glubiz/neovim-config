83703d356b881d370d1f0d32ad56781378eb5c11
Jonathan Solskov Jensen <josj@cego.dk>
2024-05-24 11:27:33 +0200

    Adding trouble and blankline

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,7 @@
 return {
   { import = "plugins.specs.crates" },
+  { import = "plugins.specs.indent-blankline" },
   { import = "plugins.specs.lspsaga" },
   { import = "plugins.specs.rustaceanvim" },
+  { import = "plugins.specs.trouble" },
 }
